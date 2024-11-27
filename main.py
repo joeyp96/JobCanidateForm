db0d7e9e609e84e4c5670abf6341bda7074ed96c
joeyp96 <josephpignatone@gmail.com>
2024-11-27 12:47:15 -0500

    commit 3: connection to api is now working.

--- main.py
+++ main.py
@@ -1,53 +1,27 @@
 import requests
-
 from requests.auth import HTTPBasicAuth
-import sqlite3
-import json
 
 
-def database():
-    # try:
-    #     db_connection = sqlite3.connect('candidate.db')
-    #     cursor = db_connection.cursor()
-    #     query_create_can_data = """CREATE TABLE IF NOT EXISTS can_data(
-    #                              name TEXT,
-    #                              email TEXT,
-    #                              DOB TEXT,
-    #                              phone TEXT,
-    #                              field TEXT,
-    #                              graduate TEXT,
-    #                              years TEXT,
-    #                              website TEXT,
-    #                              ;"""
-    #     cursor.execute(query_create_can_data)
-    #     results = cursor.fetchall()
-    #
-    #     query_delete = """"DELETE FROM can_data"""
-    #     cursor.execute(query_delete)
-    # except sqlite3.Error as db_error:
-    #     print(f'A database error: {db_error}')
-    # finally:
-    #     if db_connection:
-    #         db_connection.close()
+def call_api():
+    url = 'https://joeyp96.wufoo.com/api/v3/forms/employee-complaint-form/entries.json'
 
     with open('secret.txt', 'r') as file:
         api_key = file.read().strip()
-    candidate_list = call_api(api_key)
-    return candidate_list
 
-def call_api(api_key):
-    url = 'https://joeyp96.wufoo.com/forms/employee-complaint-form/entries/json'
     response = requests.get(url, auth=HTTPBasicAuth(api_key, 'pass'))
-    print(response.status_code)
+
+    print(f"Status Code: {response.status_code}")
+
     if response.status_code == 200:
+        print("API Connection Successful")
         data = response.json()
-        print(data)
-        print(type(data))
-        print(data[2])
-        print(data[2]['mass'])
+        print(data)  # Print the received JSON data for verification
         return data
+    elif response.status_code == 404:
+        print("Error 404")
+    else:
+        print(f"Error: Received status code {response.status_code}")
 
 
 if __name__ == '__main__':
-    data = database()
-
+    call_api()

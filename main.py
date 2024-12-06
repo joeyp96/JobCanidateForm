353a6fd4ac3ff1943209c83bb7b970809b7ef94d
Jake <j1rebell@bridgew.student.edu>
2024-12-05 19:50:18 -0500

    Cleaning up code spacing and candidate output spacing.

--- main.py
+++ main.py
@@ -3,17 +3,12 @@ import requests
 from requests.auth import HTTPBasicAuth
 import PySimpleGUI as sg
 
-
 def call_api():
     url = 'https://joeyp96.wufoo.com/api/v3/forms/employee-complaint-form/entries.json'
-
     with open('secret.txt', 'r') as file:
         api_key = file.read().strip()
-
     response = requests.get(url, auth=HTTPBasicAuth(api_key, 'pass'))
-
     print(f"Status Code: {response.status_code}")
-
     if response.status_code == 200:
         print("API Connection Successful")
         data = response.json()
@@ -24,12 +19,10 @@ def call_api():
         print(f"Error: Received status code {response.status_code}")
     return []
 
-
 def create_database():
     # connecting to database
     conn = sqlite3.connect("candidates.db")
     cursor = conn.cursor()
-
     # creating table with Wufoo fields
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS candidates (
@@ -47,9 +40,7 @@ def create_database():
     conn.commit()
     cursor.execute("DELETE FROM candidates")
     return conn
-
     # populating table with the data submitted on the forms
-
 
 def guiLayout():
     # Creates the layout of the GUI, which creates text boxes to display user data and input our filters, as well as
@@ -63,24 +54,22 @@ def guiLayout():
         [sg.Text('College Graduate? > '),
          sg.Combo(['Yes', 'No', 'Both'], default_value='Both', key='grad_key'),
          ],
-
     ]
     # Create the Window
     window = sg.Window('Job Sorter', layout, size=(1280, 720))
-
     while True:
         event, values = window.read()
         if event == sg.WIN_CLOSED or event == 'EXIT':
             break
         if event == 'Apply Filter':
             window['Candidate Box'].update('')
-            window['Candidate Box'].print(f"{'name':^40}{'email':^40}{'dob':^40}{'#':^40}{'Field of Work':^40}{'Experience':^40}{'Website':^40}{'College':^40}")
+            window['Candidate Box'].print(f"{'name':<40}{'email':<40}{'dob':<40}{'#':<40}{'Field of Work':<40}{'Experience':<40}{'Website':<40}{'College':<40}")
+            #Used to clear the output then print a header across the top including candidate attributes.
             can_list = []
             try:
                 can_list = fetch_data(int(values["min_years_of_experience"]), values["grad_key"])
             except:
                 can_list = fetch_data(0, values["grad_key"])
-
             for row in can_list:
                 id = int(row[0])
                 name = row[1]
@@ -91,17 +80,17 @@ def guiLayout():
                 experience = int(row[6])
                 website = row[7]
                 college = row[8]
-                window['Candidate Box'].print(f"{name:^40}{email:^40}{birth:^40}{phone_number:^40}{field_of_work:^40}{experience:^40}{website:^40}{college:^40}")
-
+                window['Candidate Box'].print(f"{name:<40}{email:<40}{birth:<40}{phone_number:<40}{field_of_work:<40}{experience:<40}{website:<40}{college:<40}")
+                # Taking the candidate list that was populated by fetch_data and prints out each attribute with spacing.
 
 def fetch_data(experience_filter, college_filter):
-    # Connect to the database and use the candidate information to create a list using fetchall.
     conn = sqlite3.connect("candidates.db")
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM candidates")
     data = cursor.fetchall()
     formatted_data = []
     for row in data:
+        # Grabs data from sqlite database and formats it into a tuple to be stored into a list.
         id = int(row[0])
         name = row[1]
         email = row[2]
@@ -112,11 +101,13 @@ def fetch_data(experience_filter, college_filter):
         website = row[7]
         college = row[8]
         if college_filter == 'Both':
+            # To allow the college graduate filter to work in both removing the option to filter in this statement.
             if experience >= experience_filter:
                 candidate = (id, name, email, birth, phone_number, field_of_work, experience, website, college)
                 formatted_data.append(candidate)
         else:
             if experience >= experience_filter and college == college_filter:
+                # If college experience filter is set to any option besides 'Both', this statement will choose the filter.
                 candidate = (id, name, email, birth, phone_number, field_of_work, experience, website, college)
                 formatted_data.append(candidate)
     conn.close()
@@ -125,7 +116,6 @@ def fetch_data(experience_filter, college_filter):
 
 def filter_and_insert_data(entries, conn):
     cursor = conn.cursor()
-
     for entry in entries:
         # mapping the fields from the terminal to the required format
         candidate_data = (
@@ -147,9 +137,7 @@ def filter_and_insert_data(entries, conn):
                 website, college_graduate
             ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
         ''', candidate_data)
-
     conn.commit()
-
 
 if __name__ == '__main__':
     # fetch data from API
